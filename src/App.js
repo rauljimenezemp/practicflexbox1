493dd61682102d4df0aa5062640538a2f20b9b28
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2022-01-09 17:20:57 -0600

    Agregue estilos css.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="padre">
+        <header class="header">
+          <div class="menu">
+            <div class="logo"></div>
+            <nav class="nav"></nav>
+            <div class="social"></div>
+          </div>
+          <div class="texto-principal">
+            <h1>-</h1>
+          </div>
+        </header>
+        <section class="section">
+          <div class="articulos">
+            <article class="article"></article>
+            <article class="article"></article>
+            <article class="article"></article>
+            <article class="article"></article>
+            <nav class="navegacion"></nav>
+          </div>
+          <aside class="aside">
+            <div class="publicidad"></div>
+            <div class="publicidad"></div>
+          </aside>
+        </section>
+        <footer class="footer">
+          <nav class="pie"></nav>
+        </footer>
+      </div>
 
     </div>
   );
